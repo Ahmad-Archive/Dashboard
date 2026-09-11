69a06b4c8dd87216bc0ab92543f8b76aa12504e1
Ahmad-Archive <ahmadwildansyaroni@gmail.com>
2026-09-12 04:31:35 +0700

    chore: minor tweaks to global stylesheet and App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import Sidebar from './components/layout/Sidebar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.css'
+import Dashboard from '@/pages/DashboardPage'
+import Login from '@/pages/LoginPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Router>
-      <Sidebar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </Router>
   )
 }
