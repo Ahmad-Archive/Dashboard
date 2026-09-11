4809234ce654b19b29c93607462ddb0317b1018b
Ahmad-Archive <ahmadwildansyaroni@gmail.com>
2026-09-12 04:33:24 +0700

    feat: integrate sidebar component into dashboard layout

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,14 @@
+import { Link, Route } from 'react-router-dom'
+import Sidebar from '@/components/layout/Sidebar'
+
+export default function Dashboard() {
+  return (
+    <>
+      <Sidebar />
+
+      <div>
+
+      </div>
+    </>
+  )
+}
